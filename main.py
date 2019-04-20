b38fa84f281a0e90d04a8a206664b1724cbcd9f1
aijey <artur.sydoran@gmail.com>
2019-04-20 23:30:55 +0300

    test

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def getList(song,message):
     global pos
     global transChatId
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url = "https://music.xn--41a.ws/search/"+song+"/"
+    url = "http://music.xn--41a.ws/search/"+song+"/"
     print(url)
     htmlContent = requests.get(url, headers=headers)
     print(htmlContent)
