2cafe98407e1cd349afe24bd839cf608b65e8a2b
aijey <artur.sydoran@gmail.com>
2019-04-24 17:32:37 +0300

    test

--- Data/Data.py
+++ Data/Data.py
@@ -20,7 +20,6 @@ class ParamsTable:
             self.connection.commit()
             cursor.close()
             print("Successful initialization of Params")
-            self.connection.close()
         except (Exception, psycopg2.Error) as er:
             print(er)
 
@@ -30,14 +29,13 @@ class ParamsTable:
             commands = """
             SELECT value
             FROM params
-            WHERE var_name = 'schedule_message_id';
+            WHERE var_name = 'schedule_message_id';;
             """
             cursor.execute(commands)
             message_id = cursor.fetchall()[0]
             print("Got: schedule_message_id = " + message_id + " from params table")
 
             cursor.close()
-            self.connection.close()
             return message_id
         except (Exception, psycopg2.Error) as er:
             print(er)
@@ -54,7 +52,6 @@ class ParamsTable:
             cursor.execute(commands, (message_id,))
             self.connection.commit()
             cursor.close()
-            self.connection.close()
             print("Successfully saved")
         except (Exception, psycopg2.Error) as er:
             print(er)
